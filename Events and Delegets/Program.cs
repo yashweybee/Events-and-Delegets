94f82ceed7f01139334d1f19785df19944089485
yashweybee <yashparmar@weybee.in>
2023-10-05 10:33:44 +0530

    Add project files.

---
+++ Events and Delegets/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+internal class Program
+{
+    delegate void printDelegate(string text);
+
+    public void printAnything(printDelegate p)
+    {
+        p("asdfasdasdfasdfas");
+    }
+
+
+    private static void Main(string[] args)
+    {
+        printDelegate p1 = A.TextPrint;
+        printDelegate p2 = B.MyTextPrint;
+
+        printDelegate p3 = new printDelegate(B.MyTextPrint);
+        //p3("hajs");
+        B b = new B();
+        printDelegate p5 = b.greeting;
+
+
+
+    }
+
+}
+
+public class A
+{
+    public static void TextPrint(string txt)
+    {
+        Console.WriteLine(txt);
+    }
+}
+public class B
+{
+    public static void MyTextPrint(string txt)
+    {
+        Console.WriteLine(txt);
+    }
+
+    public void greeting(string txt)
+    {
+        Console.WriteLine("gm!! " + txt);
+    }
+}
